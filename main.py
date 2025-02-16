4bfafed0ae29326dacde37d5a3d8e3c4727ad9ef
Er. Kiran Kumar <kk9840042@gmail.com>
2025-02-16 12:51:50 +0530

    Problem 1

---
+++ main.py
@@ -0,0 +1,13 @@
+# Python 100 Programs  
+
+# 1. Write a program to print Hello World.
+print("Hello World")
+
+# 2. Simple Calculator 
+def add(num1,num2):
+  add = num1 + num2
+  subtract = num1 - num2 
+  multiply = num1 * num2
+  divide = num1 / num2
+  return num1 + num2 
+print(add(3,4))
