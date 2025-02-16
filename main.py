81a847f23898fc2fdd5969e017000db1844bd1ad
Er. Kiran Kumar <kk9840042@gmail.com>
2025-02-16 13:04:56 +0530

    Problem 3

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ def calculator(num1,num2):
   subtract = num1 - num2 
   multiply = num1 * num2
   divide = num1 / num2
-  return add,subtract,multiply,divide
+  return "Sum : ",add, "Subtract :", subtract, "Multiply :", multiply, "Division :",divide
 
 print(calculator(10,5))
+
+
+# 3. Factorial of a Number 
+
+def factorial(num):
+  if num == 0:
+    return 1 
+  else: 
+    return num * factorial(num-1)
+
+number = int(input("Enter a Number : "))
+print(factorial(number))
+  
